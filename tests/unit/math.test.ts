5c16c9b6c982af81f4eba428423f91935571b69a
Mikaël Bouchez <mikael.bouchez@ekino.com>
2021-07-19 11:47:56 +0200

    chore(stryker): test stryker step 2 with 100% score

--- tests/unit/math.test.ts
+++ tests/unit/math.test.ts
@@ -6,7 +6,7 @@ describe("addition", () => {
     const result = addition(1, 2);
     expect(result).toBe(3);
   });
-  xit("should not add number if one parameters is missing", () => {
+  it("should not add number if one parameters is missing", () => {
     const result = addition(1, undefined);
     expect(result).toBeUndefined();
   });
